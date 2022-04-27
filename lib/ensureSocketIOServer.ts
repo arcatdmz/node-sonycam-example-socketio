a1a17dfa9c918ce7c6878927a231c7d0169515d7
Jun Kato <i@junkato.jp>
2022-04-28 02:29:44 +0900

    Minor refactoring

---
+++ lib/ensureSocketIOServer.ts
@@ -0,0 +1,27 @@
+import { Server } from "socket.io";
+import { NextApiResponseWithSocketIO } from "./NextApiResponseWithSocketIO";
+
+export function ensureSocketIOServer(
+  res: NextApiResponseWithSocketIO
+): Server | null {
+  if (!res.socket) {
+    return null;
+  }
+  if (!res.socket.server.io) {
+    console.log("*First use, starting socket.io");
+
+    const io = new Server(res.socket.server);
+
+    io.on("connection", (socket) => {
+      socket.broadcast.emit("a user connected", socket.id);
+      socket.on("hello", (msg) => {
+        socket.emit("hello", msg);
+      });
+    });
+
+    res.socket.server.io = io;
+  } else {
+    console.log("socket.io already running");
+  }
+  return res.socket.server.io;
+}
--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -1,26 +1,9 @@
 import type { NextApiRequest } from "next";
-import { Server } from "socket.io";
 import { NextApiResponseWithSocketIO } from "../../lib/NextApiResponseWithSocketIO";
+import { ensureSocketIOServer } from "../../lib/ensureSocketIOServer";
 
 const ioHandler = (_req: NextApiRequest, res: NextApiResponseWithSocketIO) => {
-  if (res.socket) {
-    if (!res.socket.server.io) {
-      console.log("*First use, starting socket.io");
-
-      const io = new Server(res.socket.server);
-
-      io.on("connection", (socket) => {
-        socket.broadcast.emit("a user connected", socket.id);
-        socket.on("hello", (msg) => {
-          socket.emit("hello", msg);
-        });
-      });
-
-      res.socket.server.io = io;
-    } else {
-      console.log("socket.io already running");
-    }
-  }
+  ensureSocketIOServer(res);
   res.end();
 };
 
